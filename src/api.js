3f5e1b0c7cb70f00464131cf1bcad5f0df76e122
kartikkaushik3011 <kartikkaushik3011@gmail.com>
2025-03-05 15:17:15 +0530

    Introduced Login

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://room-and-lab-allocation-backend-1.onrender.com',
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true
 });
 
